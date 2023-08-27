732a5bd1559f826450d4b167e9bdf641ecf1d15e
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-08-27 21:54:15 +0900

    イメージに画像を追加したい

--- syoryuken/ViewController.swift
+++ syoryuken/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var anime: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func button(_ sender: Any) {
+        let images: [UIImage] = [
+            UIImage(named: "attack1")!,
+            UIImage(named: "attack2")!,
+            UIImage(named: "attack3")!,
+            // 省略...
+            UIImage(named: "attack20")!
+        ]
 
+        anime.image = images
+    }
+    
 }
 
